a60e21842be4d00c24876a65202712db332f8023
mauroelectromel <electromelnqn@gmail.com>
2026-05-20 03:03:05 -0300

    Update CACHE_VERSION to 'electromel-radar-v6-r4'

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 
 'use strict';
 
-const CACHE_VERSION = 'electromel-radar-v6-r3';
+const CACHE_VERSION = 'electromel-radar-v6-r4';
 
 /* -----------------------------------------------------------------------
    Assets para pre-cachear en install.
